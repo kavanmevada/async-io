c06df0eb504ba52b22629de28dc845483aa6cfea
Kavan Mevada <kavanmevada@gmail.com>
2022-11-30 00:32:53 +0000

    version 0.0.1-alpha

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,24 @@ impl<F: Future> Unpin for FutThread<F> {}
 
 pub fn spawn<F>(fut: F) -> FutThread<F>
 where
-    F: Future,
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
 {
-    FutThread(None, Some(fut))
+    let thread = std::thread::current();
+    let waker = waker_fn::waker_fn(move || thread.unpark());
+
+    if let Some(w) = Some(waker) {
+        FutThread(
+            Some(::std::thread::spawn(move || {
+                let res = block_on(fut);
+                w.wake_by_ref();
+                res
+            })),
+            None,
+        )
+    } else {
+        FutThread(None, Some(fut))
+    }
 }
 
 impl<F> Future for FutThread<F>
@@ -29,7 +44,10 @@ where
     type Output = F::Output;
 
     fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("check point reached");
+
         let Self(handle, fut) = core::pin::Pin::get_mut(self);
+
         match (handle.take(), fut.take()) {
             (None, Some(fut)) => {
                 let waker_arc = _cx.waker().clone();
@@ -75,16 +93,36 @@ pub fn block_on<F: Future>(fut: F) -> F::Output {
 #[cfg(test)]
 mod tests {
 
-    #[test]
-    fn it_works() {
-        let res = super::block_on(async {
-            super::spawn(async {
-                println!("msg from spawned thread #2 !");
-                ::core::future::poll_fn(|_| ::core::task::Poll::<()>::Pending).await
-            })
-            .await
-        });
+    // #[test]
+    // fn it_works() {
+    //     let res = super::block_on(async {
+    //         super::spawn(async {
+    //             println!("msg from spawned thread #2 !");
+    //             // ::core::future::poll_fn(|_| ::core::task::Poll::<()>::Pending).await
+    //         })
+    //         .await
+    //     });
 
-        dbg!(&res);
+    //     dbg!(&res);
+    // }
+
+    #[test]
+    fn functional() {
+        super::block_on(async {
+            let (tx, rx) = flume::bounded::<&str>(1);
+
+            let t = super::spawn(async move {
+                while let Ok(msg) = rx.recv_async().await {
+                    println!("Received: {}", msg);
+                }
+            });
+
+            tx.send_async("Hello, world!").await.unwrap();
+            tx.send_async("How are you today?").await.unwrap();
+
+            drop(tx);
+
+            t.await;
+        });
     }
 }
